3da72c8c0e6e33f9737e3742f2abb9d492c1cf30
Nikita Nakleskin <nikonsok@gmail.com>
2022-09-15 08:07:01 +0200

    Palindrome numbers

---
+++ src/com/epam/lections/PalindromeNum.java
@@ -0,0 +1,23 @@
+package com.epam.lections;
+import java.util.Scanner;
+
+public class PalindromeNum {
+    public static void main(String[] args) {
+        IsPalNum(0);
+    }
+    public static void IsPalNum (int x) {
+        int del_num,sum=0,temp_num;
+        temp_num=x;
+        while(temp_num>0){
+            del_num=temp_num%10;
+            sum=(sum*10)+del_num;
+            temp_num=temp_num/10;
+        }
+        if(temp_num==sum)
+            System.out.println(x + " is palindrome number");
+        else
+            System.out.println(x + " isn't palindrome number");
+    }
+    }
+
+
